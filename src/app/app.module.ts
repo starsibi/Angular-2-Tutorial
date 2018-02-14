42b2e7293b9e9ec644f07993ab0ec9b020fbf589
Prithiviraj Ramalingam <Prithiviraj.Ramalingam@pfcb.com>
2018-02-14 14:25:58 +0530

    CRUD Operation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { FormsModule } from'@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
-import{ EmployeeAge }from'./employee/employeeage.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    EmployeeAge
   ],
   imports: [
     BrowserModule,
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,20 +1,61 @@
-import{Component} from'@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'my-employee',
-  templateUrl:'/employee.component.html',
-  styleUrls:['./employee.component.css']
+  selector: 'my-employee',
+  templateUrl: '/employee.component.html',
+  styleUrls: ['./employee.component.css']
 })
 
-export class EmployeeComponent{
- employee : object={
-  firstname:"",
-  lastname:"",
-  age:"",
-  phonenumber:""
- } 
- onsubmit(employeform){
-   debugger
-    console.log(JSON.stringify(employeform.form.value));
- }
+
+export class EmployeeComponent {
+  employee: object = {
+    firstname: "",
+    lastname: "",
+    age: "",
+    phonenumber: ""
+  }
+  //Condition for update button display.
+  updatebutton: boolean;
+
+  //Array of Object Creation to store the employee details
+  public employees: Array<any> = [];
+
+  //function while clikcing on the submit button.
+  onsubmit(employeform) {
+    this.employees.push(employeform.form.value);
+    employeform.reset();
+    alert("Created Successfully");
+  }
+
+  //function to delete the employee detail
+  deleteEmployee(employee) {
+    for (var i = 0; i < this.employees.length; i++) {
+      if (employee.firstname == this.employees[i].firstname) {
+        delete this.employees[i];
+        alert("Deleted Successfully");
+      }
+      if (this.employees.length == 1) {
+        this.employees.length = 0;
+      }
+    }
+  }
+  
+  //function will call once we click on update button on the table
+  updateEmployee(employee) {
+    this.updatebutton = true;
+    this.employee = employee;
+  }
+
+  //function will call once we need to update the form.
+  update(employeeform) {
+    var employee = employeeform.form.value;
+    for (var i = 0; i < this.employees.length; i++) {
+      if (employee.firstname == this.employees[i].firstname) {
+        this.employees[i] = employee;
+        this.updatebutton = false;
+        employeeform.reset();
+        alert("updated Successfully");
+      }
+    }
+  }
 }
